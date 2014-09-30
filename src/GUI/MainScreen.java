85b60b384c78a0c87a6fd4edd13ab97769989237
silkwormghh <silkworm_4@hotmail.com>
2014-09-30 15:51:57 +0800

    Rewrote everything into main function

--- src/GUI/MainScreen.java
+++ src/GUI/MainScreen.java
@@ -1,122 +1,71 @@
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Event;
-import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
-import org.eclipse.wb.swt.SWTResourceManager;
 
-public class MainScreen extends Composite {
-
-    private static final int TASK_PANE_WIDTH = 10;
-    private static final int TWO_COLUMNS = 2;
+public class MainScreen {
+    /**
+     * private static final int TASK_PANE_WIDTH = 10; private static final int
+     * TWO_COLUMNS = 2; private static final String LINE_SEPERATOR = System
+     * .getProperty("line.separator"); private static final String
+     * MESSAGE_WELCOME = "Welcome to Haystack!" + LINE_SEPERATOR +
+     * "Enter “help” for more information."; private static final String
+     * MESSAGE_TYPE_HERE = "Type commands here. Press Enter when done."; private
+     * static final String MESSAGE_UPCOMING_TASKS = "Upcoming tasks:";
+     **/
     private static final String LINE_SEPERATOR = System
             .getProperty("line.separator");
     private static final String MESSAGE_WELCOME = "Welcome to Haystack!"
             + LINE_SEPERATOR + "Enter “help” for more information.";
-    private static final String MESSAGE_TYPE_HERE = "Type commands here. Press Enter when done.";
-    private static final String MESSAGE_UPCOMING_TASKS = "Upcoming tasks:";
-
-    /**
-     * Create the composite.
-     * 
-     * @param parent
-     * @param style
-     */
-    public MainScreen(Composite parent, int style) {
-        super(parent, style);
-
-        // sets background of window as blue
-        setBackground(SWTResourceManager.getColor(185, 209, 234));
-        GridLayout gridLayout = new GridLayout();
-        gridLayout.numColumns = TWO_COLUMNS * TASK_PANE_WIDTH;
-        setLayout(gridLayout);
-
-        Text displayScreen = new Text(this, SWT.READ_ONLY | SWT.MULTI
-                | SWT.BORDER | SWT.V_SCROLL);
-
-        GridData displayScreenLayout = new GridData();
-        setUpTopScreen(displayScreenLayout, TWO_COLUMNS);
-
-        displayScreen.setLayoutData(displayScreenLayout);
-        displayScreen.setText(MESSAGE_WELCOME);
-
-        Text taskPane = new Text(this, SWT.READ_ONLY | SWT.MULTI | SWT.BORDER
-                | SWT.V_SCROLL);
-
-        GridData taskPaneLayout = new GridData();
-        setUpTopScreen(taskPaneLayout, TASK_PANE_WIDTH);
-
-        taskPane.setLayoutData(taskPaneLayout);
-        taskPane.setText(MESSAGE_UPCOMING_TASKS);
-        --taskPane.setBounds(10, 10, 100, 100);
-
-        Text userInputBox = new Text(this, SWT.BORDER);
-        GridData inputBoxLayout = new GridData();
-        setUpBottomScreen(displayScreenLayout, inputBoxLayout);
-
-        userInputBox.setLayoutData(inputBoxLayout);
-        userInputBox.setText(MESSAGE_TYPE_HERE);
-
-        userInputBox.addListener(SWT.Traverse, new Listener() {
-
-            @Override
-            public void handleEvent(Event event) {
-                if (event.detail == SWT.TRAVERSE_RETURN) {
-                    // To get user input and call another class
-                    // get output and print to screen
-                    System.out.println("Enter is pressed");
-                    String input = ((Text) event.widget).getText();
-                    System.out.println(input);
-
-                    String output = ResultGenerator.sendInput(input);
-                }
-            }
-        });
-
-    }
-
-    private void setUpBottomScreen(GridData displayScreenLayout,
-            GridData inputBoxLayout) {
-        inputBoxLayout.horizontalSpan = TASK_PANE_WIDTH + TASK_PANE_WIDTH
-                + TASK_PANE_WIDTH;
-        inputBoxLayout.horizontalAlignment = GridData.FILL;
-        displayScreenLayout.grabExcessHorizontalSpace = true;
-    }
-
-    private void setUpTopScreen(GridData screenLayout, int width) {
-        screenLayout.horizontalSpan = width;
-        screenLayout.horizontalAlignment = GridData.FILL;
-        screenLayout.grabExcessHorizontalSpace = true;
-        screenLayout.grabExcessVerticalSpace = true;
-    }
-
-    @Override
-    protected void checkSubclass() {
-        // Disable the check that prevents subclassing of SWT components
-    }
+    private static final String PROGRAM_NAME = "HayStack";
+    private static final String TASK_PANE_LABEL = "Upcoming Tasks";
+    private static final int MIN_WIDTH_SCREEN = 800;
+    private static final int MIN_HEIGHT_SCREEN = 700;
 
     public static void main(String[] args) {
         Display display = new Display();
-
         Shell shell = new Shell(display);
-        MainScreen screen = new MainScreen(shell, SWT.NONE);
-        // the layout manager handle the layout
-        // of the widgets in the container
+        shell.setText(PROGRAM_NAME);
+        GridLayout layout = new GridLayout();
+        layout.numColumns = 2;
+        shell.setLayout(layout);
+        // rearranges if theres no space
 
-        // start
+        Text displayScreen = new Text(shell, SWT.MULTI | SWT.BORDER
+                | SWT.READ_ONLY);
+        GridData gridData = new GridData(GridData.FILL_BOTH);
+        gridData.widthHint = MIN_WIDTH_SCREEN;
+        gridData.heightHint = MIN_HEIGHT_SCREEN;
+        displayScreen.setLayoutData(gridData);
+        displayScreen.setText(MESSAGE_WELCOME);
 
+        Group taskPaneGroup = new Group(shell, SWT.NULL);
+        taskPaneGroup.setText(TASK_PANE_LABEL);
+        layout = new GridLayout();
+        gridData = new GridData(GridData.FILL_BOTH);
+        gridData.widthHint = MIN_WIDTH_SCREEN;
+        gridData.heightHint = MIN_HEIGHT_SCREEN;
+        taskPaneGroup.setLayout(layout);
+
+        Text upcomingTasks = new Text(taskPaneGroup, SWT.MULTI | SWT.READ_ONLY
+                | SWT.BORDER);
+        gridData = new GridData(GridData.FILL_BOTH);
+        upcomingTasks.setLayoutData(gridData);
+
+        Text commandLine = new Text(shell, SWT.SINGLE | SWT.BORDER_SOLID);
+        gridData = new GridData(GridData.HORIZONTAL_ALIGN_FILL);
+        gridData.horizontalSpan = 2;
+        commandLine.setLayoutData(gridData);
         // end
-        screen.pack();
         shell.pack();
         shell.open();
         while (!shell.isDisposed()) {
             if (!display.readAndDispatch())
                 display.sleep();
         }
-        display.dispose();
+        displayScreen.dispose();
     }
 }
