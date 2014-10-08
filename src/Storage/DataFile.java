fe814cb21366e2c77615e2238d295d49ea9f943e
Pierce <pierceanderson.fu+github@gmail.com>
2014-10-08 17:56:09 +0800

    Changed List getters into read only & ArrayLists into Lists where relevant
    Changed List getters to return unmodifiable lists to reflect expected
    read only behaviour. Corrected references to ArrayList implementation
    types into List interface types.

--- src/Storage/DataFile.java
+++ src/Storage/DataFile.java
@@ -4,7 +4,9 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.List;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 import Parser.Parser;
@@ -13,27 +15,27 @@ public class DataFile {
     
     final private static String FILENAME = "Data";
     
-    // A task can only exist in one of the three (todo, done, deleted)
-    // arrayLists at any one time.
+    // A task can only exist in one of the three 
+    // (todo/done/deleted) lists at any one time.
     // allTasks contains references to all tasks, sorted by id
-    private static ArrayList<Task> allTasks = new ArrayList<Task>();
-    private static ArrayList<Task> toDoTasks = new ArrayList<Task>();
-    private static ArrayList<Task> doneTasks = new ArrayList<Task>();
-    private static ArrayList<Task> deletedTasks = new ArrayList<Task>();
+    private static List<Task> allTasks = new ArrayList<Task>();
+    private static List<Task> toDoTasks = new ArrayList<Task>();
+    private static List<Task> doneTasks = new ArrayList<Task>();
+    private static List<Task> deletedTasks = new ArrayList<Task>();
     
-    // ArrayList getters
+    // Task list getters
     // Aids in searching and display commands
-    public ArrayList<Task> getAllTasks() {
-        return allTasks;
+    public List<Task> getAllTasks() {
+        return Collections.unmodifiableList(allTasks);
     }
-    public ArrayList<Task> getToDoTasks() {
-        return toDoTasks;
+    public List<Task> getToDoTasks() {
+        return Collections.unmodifiableList(toDoTasks);
     }
-    public ArrayList<Task> getDoneTasks() {
-        return doneTasks;
+    public List<Task> getDoneTasks() {
+        return Collections.unmodifiableList(doneTasks);
     }
-    public ArrayList<Task> getDeletedTasks() {
-        return deletedTasks;
+    public List<Task> getDeletedTasks() {
+        return Collections.unmodifiableList(deletedTasks);
     }
     
     // Constructor
@@ -51,7 +53,7 @@ public class DataFile {
         }
     }
     
-    // Populate task arrayLists with data from system file
+    // Populate task lists with data from system file
     private void getTasksFromFile(File file) {
         try {
             Scanner scanner = new Scanner(file);
@@ -79,8 +81,8 @@ public class DataFile {
         return task;
     }
     
-    // Given id, return task object from given arrayList
-    private Task searchTaskById(ArrayList<Task> tasks, int id) {
+    // Given id, return task object from given list
+    private Task searchTaskById(List<Task> tasks, int id) {
         for (Task tempTask : tasks) {
             if (tempTask.getId() == id) {
                 return tempTask;
@@ -96,7 +98,7 @@ public class DataFile {
         return updateFile();
     }
     
-    // Refreshes system file with new data in arrayLists
+    // Refreshes system file with new data in todo and done lists
     public boolean updateFile() {
         // To write to file
         // Deleted tasks are not written to file
@@ -119,7 +121,7 @@ public class DataFile {
         return text;
     }
     
-    // Writes todo and done arrayLists to system file
+    // Writes todo and done lists to system file
     private boolean writeToFile(String newFileText) {
         try {
             File file = new File(FILENAME);
@@ -139,8 +141,8 @@ public class DataFile {
     }
     
     // Given id, deletes task object
-    // Task object is removed from todo or done arrayList,
-    // and added to deleted arrayList
+    // Task object is removed from todo or done list,
+    // and added to deleted list
     public boolean deleteTask(int id) {
         Task tempTask = searchTaskById(allTasks, id);
         if (tempTask == null) {
@@ -162,7 +164,7 @@ public class DataFile {
     
     // Deletes all task objects
     // All tasks are removed from todo and done
-    // arrayLists, and added to deleted arrayList
+    // lists, and added to deleted list
     public boolean deleteAll() {
         for (Task tempTask : allTasks) {
             if (!tempTask.isDeleted()) {
