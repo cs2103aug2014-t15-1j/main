428e8d0ef849b23fd5c6035b23cfa7014cb75236
sharon <silkworm_4@hotmail.com>
2014-10-17 01:29:45 +0800

    added upcoming tasks to taskList

--- src/GUI/TaskListUI.java
+++ src/GUI/TaskListUI.java
@@ -11,6 +11,7 @@ import Storage.Task;
  */
 public class TaskListUI {
     private static final String LIST_HEADER_SOMEDAY = "SOMEDAY";
+    private static final String LIST_HEADER_UPCOMING = "UPCOMING";
     private static final String DOT_AND_SPACE = ". ";
     private static final String LINE_SEPARATOR = System.getProperty("line.separator");
     
@@ -20,12 +21,21 @@ public class TaskListUI {
     
     private void update(){
         Label taskList = getTaskList();
-        List<Task> tasks = getTasks();
-        String floatingList = getFloatingList(tasks);
-        taskList.setText(floatingList);
+        List<Task> upcomingTasks = getUpcomingTasks();
+        List<Task> floatingTasks = getFloatingTasks();
+        String upcomingList = getStringList(upcomingTasks, LIST_HEADER_UPCOMING); // to implement: overdue tasks in red
+        
+        String floatingList = getStringList(floatingTasks, LIST_HEADER_SOMEDAY);
+        taskList.setText(upcomingList +  floatingList);
+    }
+  
+    private List<Task> getUpcomingTasks(){
+        Processor processor = Processor.getInstance();
+      List<Task> tasks =   processor.fetchFloatingTasks();
+      return tasks;
     }
     
-    private List<Task> getTasks(){
+    private List<Task> getFloatingTasks(){
         Processor processor = Processor.getInstance();
       List<Task> tasks =   processor.fetchFloatingTasks();
       return tasks;
@@ -37,7 +47,7 @@ public class TaskListUI {
         return taskList;
     }
     
-    private String getFloatingList(List<Task> tasks){
+    private String getStringList(List<Task> tasks, String header){
         if(!isValid(tasks)){
             return "Nothing to display";
         }
