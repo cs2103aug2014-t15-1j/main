9703e8fa9ac09cad9300212e8dcf10b37f9f8ff0
silkwormghh <silkworm_4@hotmail.com>
2014-10-19 01:21:31 +0800

    added test folder
    We can add our test cases into this folder

---
+++ test/GUI/ResultGeneratorTest.java
@@ -0,0 +1,14 @@
+package GUI;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ResultGeneratorTest {
+
+    @Test
+    public void testSendInput() {
+        fail("Not yet implemented");
+    }
+
+}
