726593deee2201ecf6ec67e66a3575eac709dc62
silkwormghh <silkworm_4@hotmail.com>
2014-10-20 00:23:10 +0800

    Moved caret to commandLine
    It's more convenient to use now but it causes table to look ugly

--- src/GUI/TaskListUI.java
+++ src/GUI/TaskListUI.java
@@ -3,6 +3,7 @@ package GUI;
 import java.util.List;
 
 import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
 
 import Logic.Processor;
 import Storage.Task;
@@ -10,8 +11,8 @@ import Storage.Task;
  * This class updates the TaskList Interface whenever a change is made. 
  */
 public class TaskListUI {
-    private static final String LIST_HEADER_SOMEDAY = "SOMEDAY";
-    private static final String LIST_HEADER_UPCOMING = "UPCOMING";
+    private static final String LIST_HEADER_SOMEDAY = "SOMEDAY:";
+    private static final String LIST_HEADER_UPCOMING = "UPCOMING:";
     private static final String DOT_AND_SPACE = ". ";
     private static final String LINE_SEPARATOR = System.getProperty("line.separator");
     
@@ -20,16 +21,17 @@ public class TaskListUI {
     }
     
     private void update(){
-        Label taskList = getTaskList();
+        Text taskList = getTaskList();
         List<Task> upcomingTasks = getUpcomingTasks();
         List<Task> floatingTasks = getFloatingTasks();
         String upcomingList = getStringList(upcomingTasks, LIST_HEADER_UPCOMING); // to implement: overdue tasks in red
         
         String floatingList = getStringList(floatingTasks, LIST_HEADER_SOMEDAY);
-        taskList.setText(upcomingList +  floatingList);
+        taskList.setText(upcomingList + LINE_SEPARATOR +  floatingList);
     }
   
     private List<Task> getUpcomingTasks(){
+       
         Processor processor = Processor.getInstance();
       List<Task> tasks =   processor.fetchFloatingTasks();
       return tasks;
@@ -41,17 +43,17 @@ public class TaskListUI {
       return tasks;
     }
     
-    private Label getTaskList(){
+    private Text getTaskList(){
         SetUp setUp = SetUp.getInstance();
-        Label taskList = setUp.getTaskList();
+        Text taskList = setUp.getTaskList();
         return taskList;
     }
     
     private String getStringList(List<Task> tasks, String header){
         if(!isValid(tasks)){
-            return "Nothing to display";
+            return header + LINE_SEPARATOR + "Nothing to display" + LINE_SEPARATOR;
         }
-        String list = LIST_HEADER_SOMEDAY + LINE_SEPARATOR;
+        String list = header + LINE_SEPARATOR;
         list = list + changeTaskListToString(tasks);
         return list;
     }
