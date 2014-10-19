0447e5cca9c617b7bb1e899a54407db70ad02459
Pierce <pierceanderson.fu+github@gmail.com>
2014-10-19 19:46:55 +0800

    Updated unit test to match DateTime format

--- src/Storage/BlockDateTest.java
+++ src/Storage/BlockDateTest.java
@@ -24,81 +24,81 @@ public class BlockDateTest {
 
     @Test
     public void testConstructorStoresDateTimeArgs() throws Exception {
-        DateTime start = new DateTime("1/1/2014", "1111");
-        DateTime end = new DateTime("2/2/2014", "2222");
+        DateTime start = new DateTime("11/12/2014", "1111");
+        DateTime end = new DateTime("13/12/2014", "2222");
         BlockDate bD = new BlockDate(start, end);
-        assertEquals("Cloned start date", "1/1/2014", bD.getStartDate());
+        assertEquals("Cloned start date", "11/12/2014", bD.getStartDate());
         assertEquals("Cloned start time", "1111", bD.getStartTime());
-        assertEquals("Cloned end date", "2/2/2014", bD.getEndDate());
+        assertEquals("Cloned end date", "13/12/2014", bD.getEndDate());
         assertEquals("Cloned end time", "2222", bD.getEndTime());
         start.resetDateTime();
         end.resetDateTime();
         assertEquals("Cloned start date by value", bD.getStartDate(),
-                     "1/1/2014");
+                     "11/12/2014");
         assertEquals("Cloned start time by value", "1111", bD.getStartTime());
-        assertEquals("Cloned end date by value", "2/2/2014", bD.getEndDate());
+        assertEquals("Cloned end date by value", "13/12/2014", bD.getEndDate());
         assertEquals("Cloned end time by value", "2222", bD.getEndTime());
     }
 
     @Test
     public void testResetStartDate() throws Exception {
-        DateTime start = new DateTime("1/1/2014", "1111");
-        DateTime end = new DateTime("2/2/2014", "2222");
+        DateTime start = new DateTime("11/12/2014", "1111");
+        DateTime end = new DateTime("13/12/2014", "2222");
         BlockDate bD = new BlockDate(start, end);
-        assertEquals("Cloned start date", "1/1/2014", bD.getStartDate());
+        assertEquals("Cloned start date", "11/12/2014", bD.getStartDate());
         assertEquals("Cloned start time", "1111", bD.getStartTime());
-        assertEquals("Cloned end date", "2/2/2014", bD.getEndDate());
+        assertEquals("Cloned end date", "13/12/2014", bD.getEndDate());
         assertEquals("Cloned end time", "2222", bD.getEndTime());
         bD.resetStartDate();
         assertEquals("Empty start date", "", bD.getStartDate());
         assertEquals("Empty start time", "1111", bD.getStartTime());
-        assertEquals("Not empty end date", "2/2/2014", bD.getEndDate());
+        assertEquals("Not empty end date", "13/12/2014", bD.getEndDate());
         assertEquals("Not empty end time", "2222", bD.getEndTime());
     }
 
     @Test
     public void testResetStartTime() throws Exception {
-        DateTime start = new DateTime("1/1/2014", "1111");
-        DateTime end = new DateTime("2/2/2014", "2222");
+        DateTime start = new DateTime("11/12/2014", "1111");
+        DateTime end = new DateTime("13/12/2014", "2222");
         BlockDate bD = new BlockDate(start, end);
-        assertEquals("Cloned start date", "1/1/2014", bD.getStartDate());
+        assertEquals("Cloned start date", "11/12/2014", bD.getStartDate());
         assertEquals("Cloned start time", "1111", bD.getStartTime());
-        assertEquals("Cloned end date", "2/2/2014", bD.getEndDate());
+        assertEquals("Cloned end date", "13/12/2014", bD.getEndDate());
         assertEquals("Cloned end time", "2222", bD.getEndTime());
         bD.resetStartTime();
-        assertEquals("Empty start date", "1/1/2014", bD.getStartDate());
+        assertEquals("Empty start date", "11/12/2014", bD.getStartDate());
         assertEquals("Empty start time", "", bD.getStartTime());
-        assertEquals("Not empty end date", "2/2/2014", bD.getEndDate());
+        assertEquals("Not empty end date", "13/12/2014", bD.getEndDate());
         assertEquals("Not empty end time", "2222", bD.getEndTime());
     }
 
     @Test
     public void testResetStart() throws Exception {
-        DateTime start = new DateTime("1/1/2014", "1111");
-        DateTime end = new DateTime("2/2/2014", "2222");
+        DateTime start = new DateTime("11/12/2014", "1111");
+        DateTime end = new DateTime("13/12/2014", "2222");
         BlockDate bD = new BlockDate(start, end);
-        assertEquals("Cloned start date", "1/1/2014", bD.getStartDate());
+        assertEquals("Cloned start date", "11/12/2014", bD.getStartDate());
         assertEquals("Cloned start time", "1111", bD.getStartTime());
-        assertEquals("Cloned end date", "2/2/2014", bD.getEndDate());
+        assertEquals("Cloned end date", "13/12/2014", bD.getEndDate());
         assertEquals("Cloned end time", "2222", bD.getEndTime());
         bD.resetStart();
         assertEquals("Empty start date", "", bD.getStartDate());
         assertEquals("Empty start time", "", bD.getStartTime());
-        assertEquals("Not empty end date", "2/2/2014", bD.getEndDate());
+        assertEquals("Not empty end date", "13/12/2014", bD.getEndDate());
         assertEquals("Not empty end time", "2222", bD.getEndTime());
     }
 
     @Test
     public void testResetEnd() throws Exception {
-        DateTime start = new DateTime("1/1/2014", "1111");
-        DateTime end = new DateTime("2/2/2014", "2222");
+        DateTime start = new DateTime("11/12/2014", "1111");
+        DateTime end = new DateTime("13/12/2014", "2222");
         BlockDate bD = new BlockDate(start, end);
-        assertEquals("Cloned start date", "1/1/2014", bD.getStartDate());
+        assertEquals("Cloned start date", "11/12/2014", bD.getStartDate());
         assertEquals("Cloned start time", "1111", bD.getStartTime());
-        assertEquals("Cloned end date", "2/2/2014", bD.getEndDate());
+        assertEquals("Cloned end date", "13/12/2014", bD.getEndDate());
         assertEquals("Cloned end time", "2222", bD.getEndTime());
         bD.resetEnd();
-        assertEquals("Not empty start date", "1/1/2014", bD.getStartDate());
+        assertEquals("Not empty start date", "11/12/2014", bD.getStartDate());
         assertEquals("Not empty start time", "1111", bD.getStartTime());
         assertEquals("Empty end date", "", bD.getEndDate());
         assertEquals("Empty end time", "", bD.getEndTime());
@@ -106,15 +106,15 @@ public class BlockDateTest {
 
     @Test
     public void testResetEndDate() throws Exception {
-        DateTime start = new DateTime("1/1/2014", "1111");
-        DateTime end = new DateTime("2/2/2014", "2222");
+        DateTime start = new DateTime("11/12/2014", "1111");
+        DateTime end = new DateTime("13/12/2014", "2222");
         BlockDate bD = new BlockDate(start, end);
-        assertEquals("Cloned start date", "1/1/2014", bD.getStartDate());
+        assertEquals("Cloned start date", "11/12/2014", bD.getStartDate());
         assertEquals("Cloned start time", "1111", bD.getStartTime());
-        assertEquals("Cloned end date", "2/2/2014", bD.getEndDate());
+        assertEquals("Cloned end date", "13/12/2014", bD.getEndDate());
         assertEquals("Cloned end time", "2222", bD.getEndTime());
         bD.resetEndDate();
-        assertEquals("Not empty start date", "1/1/2014", bD.getStartDate());
+        assertEquals("Not empty start date", "11/12/2014", bD.getStartDate());
         assertEquals("Not empty start time", "1111", bD.getStartTime());
         assertEquals("Empty end date", "", bD.getEndDate());
         assertEquals("Empty end time", "2222", bD.getEndTime());
@@ -122,17 +122,17 @@ public class BlockDateTest {
 
     @Test
     public void testResetEndTime() throws Exception {
-        DateTime start = new DateTime("1/1/2014", "1111");
-        DateTime end = new DateTime("2/2/2014", "2222");
+        DateTime start = new DateTime("11/12/2014", "1111");
+        DateTime end = new DateTime("13/12/2014", "2222");
         BlockDate bD = new BlockDate(start, end);
-        assertEquals("Cloned start date", "1/1/2014", bD.getStartDate());
+        assertEquals("Cloned start date", "11/12/2014", bD.getStartDate());
         assertEquals("Cloned start time", "1111", bD.getStartTime());
-        assertEquals("Cloned end date", "2/2/2014", bD.getEndDate());
+        assertEquals("Cloned end date", "13/12/2014", bD.getEndDate());
         assertEquals("Cloned end time", "2222", bD.getEndTime());
         bD.resetEndTime();
-        assertEquals("Not empty start date", "1/1/2014", bD.getStartDate());
+        assertEquals("Not empty start date", "11/12/2014", bD.getStartDate());
         assertEquals("Not empty start time", "1111", bD.getStartTime());
-        assertEquals("Empty end date", "2/2/2014", bD.getEndDate());
+        assertEquals("Empty end date", "13/12/2014", bD.getEndDate());
         assertEquals("Empty end time", "", bD.getEndTime());
     }
 }
