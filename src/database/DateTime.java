d7870347257e4a4c7f80ea295f2764b1d1e83d81
Pierce <pierceanderson.fu+github@gmail.com>
2014-10-25 18:51:27 +0800

    Added equals(Object) and hashCode() to DateTime
    > Overriding Object class functions

--- src/database/DateTime.java
+++ src/database/DateTime.java
@@ -171,20 +171,97 @@ public class DateTime implements Comparable<DateTime> {
         // No differences detected
         return 0;
     }
-    
+
+    /**
+     * Indicates whether some other object is "equal to" this one.
+     * 
+     * The equals method implements an equivalence relation on non-null object
+     * references:
+     * <ul>
+     * <li>It is reflexive: for any non-null reference value x, x.equals(x)
+     * should return true.
+     * <li>It is symmetric: for any non-null reference values x and y,
+     * x.equals(y) should return true if and only if y.equals(x) returns true.
+     * <li>It is transitive: for any non-null reference values x, y, and z, if
+     * x.equals(y) returns true and y.equals(z) returns true, then x.equals(z)
+     * should return true.
+     * <li>It is consistent: for any non-null reference values x and y, multiple
+     * invocations of x.equals(y) consistently return true or consistently
+     * return false, provided no information used in equals comparisons on the
+     * objects is modified.
+     * <li>For any non-null reference value x, x.equals(null) should return
+     * false.
+     * </ul>
+     * 
+     * The equals method for class Object implements the most discriminating
+     * possible equivalence relation on objects; that is, for any non-null
+     * reference values x and y, this method returns true if and only if x and y
+     * refer to the same object (x == y has the value true).
+     * 
+     * Note that it is generally necessary to override the hashCode method
+     * whenever this method is overridden, so as to maintain the general
+     * contract for the hashCode method, which states that equal objects must
+     * have equal hash codes.
+     * 
+     * 
+     * @param obj The reference object with which to compare.
+     * 
+     * @returns True if this object is the same as the obj argument; false
+     *          otherwise.
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof DateTime)) {
+            return false;
+        } else if (obj == this) {
+            return true;
+        }
+
+        DateTime otherDateTime = (DateTime) obj;
+        return (compareTo(otherDateTime) == 0);
+    }
+
+    /**
+     * Returns a hash code value for the object. This method is supported for
+     * the benefit of hash tables such as those provided by HashMap.
+     * 
+     * The general contract of hashCode is:
+     * <ul>
+     * <li>Whenever it is invoked on the same object more than once during an
+     * execution of a Java application, the hashCode method must consistently
+     * return the same integer, provided no information used in equals
+     * comparisons on the object is modified. This integer need not remain
+     * consistent from one execution of an application to another execution of
+     * the same application.
+     * <li>If two objects are equal according to the equals(Object) method, then
+     * calling the hashCode method on each of the two objects must produce the
+     * same integer result.
+     * <li>It is not required that if two objects are unequal according to the
+     * equals(java.lang.Object) method, then calling the hashCode method on each
+     * of the two objects must produce distinct integer results. However, the
+     * programmer should be aware that producing distinct integer results for
+     * unequal objects may improve the performance of hash tables.
+     * </ul>
+     * 
+     * As much as is reasonably practical, the hashCode method defined by class
+     * Object does return distinct integers for distinct objects.
+     * 
+     * @return A hash code value for this object.
+     */
+    @Override
+    public int hashCode() {
+        return date.hashCode() + time.hashCode();
+    }
+
     /**
      * Compares this <code>DateTime</code> with an input <code>DateTime</code>.<br>
-     * Returns <code>true</code> if it is earlier than the other <code>DateTime</code>.
+     * Returns <code>true</code> if it is earlier than the other
+     * <code>DateTime</code>.
      * 
      * @author Yeo Zi Xian, Justin
      */
     public boolean isEarlierThan(DateTime otherDateTime) {
-       return (this.compareTo(otherDateTime) < 0);
-    }
-
-    // TODO: Should this be the overriding equals()?
-    public boolean equalsTo(DateTime otherDateTime) {
-        return (compareTo(otherDateTime) == 0);
+        return (this.compareTo(otherDateTime) < 0);
     }
 
     /**
