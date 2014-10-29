c72bda32d66ad09743a7974caaae03f271eaf423
silkwormghh <silkworm_4@hotmail.com>
2014-10-29 23:06:49 +0800

    added format feedback

--- src/gui/MainScreen.java
+++ src/gui/MainScreen.java
@@ -3,6 +3,8 @@ package gui;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.CTabFolder;
 import org.eclipse.swt.custom.StyledText;
+import org.eclipse.swt.events.KeyAdapter;
+import org.eclipse.swt.events.KeyEvent;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.ImageData;
 import org.eclipse.swt.graphics.Rectangle;
@@ -31,12 +33,14 @@ public class MainScreen {
     private static final String INVALID_INPUT = "Invalid Input.";
     private static final String SUCCESSFUL_DELETE_ALL = "Erased all data!";
     private static final String UNSUCCESSFUL_DELETE_ALL = "Did not delete anything";
+    private static final String MESSAGE_TYPE_HERE = "Type here";
 
     private static final String CONFIRM = "yes";
     private static final String NO_CONFIRM = "no";
 
     private static String CODE_EXIT = "exit";
     private static boolean askConfrim = false;
+    private static boolean isFeedback = false;
 
     private static ResultGenerator resultGenerator = new ResultGenerator();
 
@@ -77,6 +81,7 @@ public class MainScreen {
         removeText(setUpScreen);
 
         try {
+            provideFeedback(setUpScreen);
             readUserInput(setUpScreen);
         } catch (Exception e) {
             String message = e.getMessage();
@@ -118,6 +123,7 @@ public class MainScreen {
     private static void removeText(SetUp screen) {
 
         final Text commandLine = screen.getCommandLine();
+        final StyledText feedback = screen.getFeedBack();
         commandLine.addListener(SWT.KeyDown, new Listener() {
             @Override
             public void handleEvent(Event event) {
@@ -128,6 +134,24 @@ public class MainScreen {
                         commandLine.removeListener(SWT.KeyDown, this);
                         break;
                 }
+            }
+        });
+    }
+
+    private static void provideFeedback(final SetUp screen) {
+        final Text commandLine = screen.getCommandLine();
+        final StyledText feedback = screen.getFeedBack();
+
+        commandLine.addKeyListener(new KeyAdapter() {
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                giveSuggestions(commandLine, feedback);
+                if (commandLine.getText().isEmpty() && isFeedback) {
+                    feedback.setText("");
+                }
+
+                isFeedback = false;
             }
 
         });
@@ -144,6 +168,7 @@ public class MainScreen {
 
                 String input = commandLine.getText();
                 String output = "";
+
                 commandLine.setText("");
                 if (askConfrim) {
                     if (!isValidDeleteAll(input)) {
@@ -178,7 +203,6 @@ public class MainScreen {
                         } else {
 
                             feedback.setText(output);
-
                         }
 
                     } catch (Exception error) {
@@ -214,4 +238,88 @@ public class MainScreen {
     private static void exitProgram() {
         System.exit(0);
     }
+
+    protected static void giveSuggestions(final Text commandLine,
+                                          final StyledText feedback) {
+        String input = commandLine.getText();
+
+        if (input.isEmpty()) {
+            return;
+        }
+
+        Character firstLetter = input.charAt(0);
+        switch (Character.toLowerCase(firstLetter)) {
+            case 'a':
+                feedback.setText("add [name] start: [DD/MM/YYYY hhmm] #tag");
+                return;
+            case 'b':
+                feedback.setText("block [DD/MM/YYYY hhmm] to [DD/MM/YYYY hhmm]");
+                return;
+            case 'd':
+                if (!isOutOfBounds(input, 1) &&
+                    Character.toLowerCase(input.charAt(1)) == 'i') {
+                    feedback.setText("display");
+                    return;
+                } else if (!isOutOfBounds(input, 1) &&
+                           Character.toLowerCase(input.charAt(1)) == 'o') {
+                    feedback.setText("done");
+                    return;
+                }
+                feedback.setText("delete [id] ");
+
+                return;
+            case 'e':
+                if (!isOutOfBounds(input, 1) &&
+                    Character.toLowerCase(input.charAt(1)) == 'x') {
+                    feedback.setText("exit");
+                    return;
+                }
+                feedback.setText("edit [id] <parameter>:  [value]");
+                return;
+            case 'h':
+                feedback.setText("help");
+                return;
+            case 'r':
+                if (!isOutOfBounds(input, 2) &&
+                    Character.toLowerCase(input.charAt(2)) == 'd') {
+                    feedback.setText("redo [id]");
+                    return;
+                } else if (!isOutOfBounds(input, 3) &&
+                           (Character.toLowerCase(input.charAt(3)) == 'e')) {
+                    feedback.setText("reset");
+                    return;
+                }
+                feedback.setText("restore [id]");
+
+                return;
+            case 's':
+                if (!isOutOfBounds(input, 1) &&
+                    Character.toLowerCase(input.charAt(1)) == 'h') {
+                    feedback.setText("show");
+                    return;
+                }
+                feedback.setText("search [id]");
+                return;
+            case 't':
+                feedback.setText("todo [id]");
+                return;
+            case 'u':
+                if (!isOutOfBounds(input, 2) &&
+                    (Character.toLowerCase(input.charAt(2)) == 'b')) {
+                    feedback.setText("unblock [DD/MM/YYYY hhmm] to [DD/MM/YYYY hhmm]");
+                    return;
+                }
+                feedback.setText("undo");
+                return;
+
+        }
+    }
+
+    protected static boolean isOutOfBounds(String input, int index) {
+        if (input.length() - 1 < index) {
+            return true;
+        }
+        return false;
+    }
+
 }
