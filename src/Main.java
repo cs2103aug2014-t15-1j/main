c66d069eb00546bb7a9256c1f87b65db6573226f
yzxj <yeozx@comp.nus.edu.sg>
2014-10-31 19:05:38 +0800

    Shifted MainScreen content back
    > I just wanted the the main function to be an auxiliary class, so that
    it is clear which file to run. Ideally this file has no logic or
    implementation whatsoever.

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import gui.MainScreen;
+
+/**
+ * This class contains the main method for Haystack (This is for ease of
+ * execution).
+ * 
+ * @author Yeo Zi Xian, Justin
+ *
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        MainScreen.run();
+    }
+
+}
