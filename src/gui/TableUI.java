cfaa4787ddf971414bc43ae1ac53f57790800cc2
sharon <silkworm_4@hotmail.com>
2014-11-05 21:40:53 +0800

    minor change
    > added block status to table

--- src/gui/TableUI.java
+++ src/gui/TableUI.java
@@ -39,6 +39,7 @@ public class TableUI extends Composite {
     private static final String PARA_STATUS_DELETED = "Deleted";
     private static final String PARA_STATUS_TODO = "To do";
     private static final String PARA_STATUS_DONE = "Done";
+    private static final String PARA_STATUS_BLOCK = "Blocked Date";
 
     // NOTE: 350 is able to fit up to 20 chars
     private static final int COL_WIDTH = 100;
@@ -51,6 +52,7 @@ public class TableUI extends Composite {
 
     // NOTE: 150 is just right for all statuses - To Do, Done, Deleted
     private static final int COL_WIDTH_STATUS = 135;
+   
 
     private TableViewer tableViewer;
     private CTabFolder folder;
@@ -236,8 +238,10 @@ public class TableUI extends Composite {
                     } else if (task.isDone()) {
                         Status = PARA_STATUS_DONE + " " +
                                  task.getCompletedOn().toString();
-                    } else {
+                    } else if(task.isToDo()){
                         Status = PARA_STATUS_TODO;
+                    }else{
+                        Status = PARA_STATUS_BLOCK;
                     }
                     return Status;
                 }
