e3f2b14b8704e73537f539210383867e8a42b97a
Pierce <pierceanderson.fu+github@gmail.com>
2014-11-06 11:06:59 +0800

    Resolves #168, added database test suite

---
+++ test/database/DatabaseTestSuite.java
@@ -0,0 +1,10 @@
+package database;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ TaskTest.class, DateTimeTest.class })
+public class DatabaseTestSuite {
+
+}
